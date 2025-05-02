ab906b2eece34dd0fb8d6ab8659e68dd06abf1f0
Dinesh7466 <127771099+Dinesh7466@users.noreply.github.com>
2025-05-02 13:27:13 +0000

    String

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,3 +4,9 @@ const repoCount = 50
 console.log(name + repoCount);
 
 console.log(`Hello My name is ${name} and my Repo Count is ${repoCount}`)
+
+const gameName = new String("Car Racing")
+
+console.log(gameName[0].__proto__)
+console.log(gameName.length) 
+console.log(gameName.toUpperCase)
