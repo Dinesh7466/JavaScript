1c9be1b6d0cd5da2060ed72c1982321004781fb3
Dinesh7466 <127771099+Dinesh7466@users.noreply.github.com>
2025-05-09 10:48:02 +0000

    for in

---
+++ 05_iterators/04_four.js
@@ -0,0 +1,28 @@
+// for in 
+const myObject = {
+    js:'javaScript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js", "rb", "py", "jsva", "cpp"]
+for (const key in programming) {
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set('In',"India")
+// map.set('USA',"United State of America")
+// map.set('Fr',"France")
+
+// for (const key in map) {    // es trah map ke uper itration nhi kiya ja sakta
+//     console.log(key);
+    
+// }
